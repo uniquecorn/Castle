601fc09fc86e898b90b1a6b812860bbfc817c398
Elijah James <uniquecorn@outlook.com>
2019-01-18 20:25:00 +0800

    Replaced primitives

--- CastleFramework/Scripts/CastleTools.cs
+++ CastleFramework/Scripts/CastleTools.cs
@@ -535,12 +535,44 @@ public class CastleTools : MonoBehaviour
             Process.Start("open", "-R " + itemPath);
         }
     }
-	public static Color ClearColor(Color _color)
-	{
-		return new Color(_color.r,_color.g,_color.b,0);
-	}
-	public static Color FullColor(Color _color)
-	{
-		return new Color(_color.r,_color.g,_color.b,1);
-	}
+}
+public static class CastleExtensions
+{
+    public static List<T> Move<T>(this List<T> list, int oldIndex, int newIndex)
+    {
+
+        // exit if possitions are equal or outside array
+        if ((oldIndex == newIndex) || (0 > oldIndex) || (oldIndex >= list.Count) || (0 > newIndex) ||
+            (newIndex >= list.Count)) return list;
+        // local variables
+        var i = 0;
+        T tmp = list[oldIndex];
+        // move element down and shift other elements up
+        if (oldIndex < newIndex)
+        {
+            for (i = oldIndex; i < newIndex; i++)
+            {
+                list[i] = list[i + 1];
+            }
+        }
+        // move element up and shift other elements down
+        else
+        {
+            for (i = oldIndex; i > newIndex; i--)
+            {
+                list[i] = list[i - 1];
+            }
+        }
+        // put element from position 1 to destination
+        list[newIndex] = tmp;
+        return list;
+    }
+    public static Color Clear(this Color _color)
+    {
+        return new Color(_color.r, _color.g, _color.b, 0);
+    }
+    public static Color Full(this Color _color)
+    {
+        return new Color(_color.r, _color.g, _color.b, 1);
+    }
 }
